b92ca02544f17e2913b5dcd48ef2fba668a5139b
Geonovum <>
2024-07-24 14:01:27 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/0510a065988815d83b5c26816f34706d58477489

---
+++ js/document-config.js
@@ -0,0 +1,26 @@
+var documentConfig =
+{
+  title: "Usecases voor 3D GeoSPARQL",
+  shortName: "3DGSP",
+  pubDomain: "wp",
+  specStatus: "wv",
+  specType: "al",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/wp/3DGSP/"
+  ],
+  publishDate: "2024-07-24",
+  publishVersion: "0.0.1",
+  thisVersion: [
+    "https://geonovum.github.io/wp-3DGSP/"
+  ],
+  edDraftURI: "https://geonovum.github.io/wp-3DGSP/",
+  canonicalURI: "https://docs.geostandaarden.nl/wp/3DGSP/",
+  authors: [
+    {
+      name: "Rolf Jonker",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ]
+}

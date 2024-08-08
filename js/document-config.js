f0c8a7a1d31f39404c8900850b8e1cbed006c0ef
Geonovum <>
2024-08-08 12:55:40 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/439db730d82372489d09d6ee5e9fc0417f9d1b7a

--- js/document-config.js
+++ js/document-config.js
@@ -56,6 +56,76 @@ var documentConfig =
       editors: "",
       date: "",
       status: ""
+    },
+    "Rietveld, 2024": {
+      title: "Afsprakenstelsel DSGO v1.0",
+      href: "https://afsprakenstelseldsgo.atlassian.net/wiki/spaces/DSGO/pages/316967452/Wat+is+het+Digitaal+Stelsel+Gebouwde+Omgeving",
+      authors: [
+        "Bauke Rietveld"
+      ],
+      publisher: "DigiGO",
+      editors: "",
+      date: "2024-06-19",
+      status: ""
+    },
+    "Digitaal Stelsel, 2024": {
+      title: "Digitaal Stelsel Gebouwde Omgeving",
+      href: "https://www.digigo.nu/digitaal-stelsel/waarom-dsgo/",
+      authors: "",
+      publisher: "DigiGO",
+      editors: "",
+      date: "2024-06-19",
+      status: ""
+    },
+    "Abhishek Bhargava et al., 2024": {
+      title: "Enabling information continuity across BIM-GIS domains: A bSI and OGC strategic roadmap",
+      href: "https://www.buildingsmart.org/wp-content/uploads/2024/05/bSI-OGC-BIM-GIS-Strategic-Roadmap_Industry_Insight_bSI_v2.pdf",
+      authors: [
+        "Abhishek Bhargava",
+        "Clive Billiald",
+        "Christophe Castaing",
+        "Michelangelo Cianciulli",
+        "Marc Goldman",
+        "Leif Granholm",
+        "Ian Howell",
+        "Richard Kelly",
+        "Jagannath Mallela",
+        "Francesca Noardo",
+        "Kimon Onuma",
+        "Dr. Erik Poirier",
+        "Michel Rives",
+        "Greg Schleusner",
+        "Scott Simmons",
+        "Ingo Simonis",
+        "Daniel Soderlindh"
+      ],
+      publisher: "buildingSMART® International Ltd.",
+      editors: "",
+      date: "2024-02-01",
+      status: ""
+    },
+    "BuildingSMART, 2023": {
+      title: "Strategic Roadmap Building Smart International",
+      href: "https://www.buildingsmart.org/about/strategic-roadmap/",
+      authors: [
+        "BuildingSmart"
+      ],
+      publisher: "",
+      editors: "",
+      date: "2023-09-01",
+      status: ""
+    },
+    "Mceachen & Lewis, 2023": {
+      title: "Enabling knowledge sharing by managing dependencies and interoperability between interlinked spatial knowledge graphs",
+      href: "https://isprs-archives.copernicus.org/articles/XLVIII-4-W7-2023/117/2023/isprs-archives-XLVIII-4-W7-2023-117-2023.pdf",
+      authors: [
+        "Nathan Mceachen",
+        "Justin Lewis"
+      ],
+      publisher: "The International Archives of the Photogrammetry, Remote Sensing and Spatial Information Sciences",
+      editors: "",
+      date: "2023-06-22",
+      status: ""
     }
   }
 }
